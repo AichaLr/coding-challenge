91d8a62284a671f0273962c9112b9e5ee77e3d19
Aicha Louraghi <a.louraghi@esi-sba.dz>
2024-02-04 01:02:57 +0100

    fix(app/purchase): update the scchema of purchase to embed some of product data to avoid joins in stats

--- app/src/purchase/purchase.service.ts
+++ app/src/purchase/purchase.service.ts
@@ -3,32 +3,32 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Purchase } from '../purchase/schemas/purchase.schema';
 import { UserService } from '@app/user';
-import { ProductService } from '../product/product.service';
 import { TopSellingProduct } from './dtos/top-selling-product.payload';
 import { PurchaseStatisticsFilterDto } from './dtos';
 import { LoggerService } from '@app/common/logger/logger.service';
+import { CreditCard } from '@app/external-api/interfaces';
+import { ExternalApiService } from '@app/external-api';
 
 @Injectable()
 export class PurchaseService {
   constructor(
     @InjectModel(Purchase.name) private purchaseModel: Model<Purchase>,
-    private readonly productService: ProductService,
+    private readonly externalApiService: ExternalApiService,
     private readonly userService: UserService,
     private readonly logger: LoggerService,
   ) {}
 
+  //TODO add typing response and verify responses for all functions also in the controller
   async purchaseStatistics(
     purchaseStatisticsFilterDto: PurchaseStatisticsFilterDto,
     userId: string,
   ) {
     const { topSelling } = purchaseStatisticsFilterDto;
     const topSellingProducts = await this.findTopSellingProducts(topSelling);
-    //const trendingProducts = await this.getTrendingProducts();
     const totalPurchases = await this.calculateTotalPurchases(userId);
     this.logger.log('PurchaseService', 'Fetching Purchase Statistics');
     return {
       topSellingProducts,
-      //trendingProducts,
       totalPurchases,
     };
   }
@@ -37,7 +37,10 @@ export class PurchaseService {
     const topSellingProducts = await this.purchaseModel.aggregate([
       {
         $group: {
-          _id: '$product',
+          _id: '$product._id',
+          name: { $first: { $ifNull: ['$product.name', ''] } },
+          price: { $first: { $ifNull: ['$product.price', 0] } },
+          category: { $first: { $ifNull: ['$product.category', ''] } },
           count: { $sum: 1 },
         },
       },
@@ -48,26 +51,18 @@ export class PurchaseService {
         $limit: limit,
       },
     ]);
-
-    const result: TopSellingProduct[] = [];
-
-    for await (const productInfo of topSellingProducts) {
-      const productId = productInfo._id;
-      const product = await this.productService.getProductById(productId);
-      result.push({
-        name: product.name,
-        id: product._id.toString(),
-        purchaseCount: productInfo.count,
-      });
-    }
-
-    return result;
+    return topSellingProducts;
   }
 
   async calculateTotalPurchases(userId: string): Promise<number> {
     const user = await this.userService.getOneById(userId);
-    const where = { user };
+    const where = { purchasedBy: user };
     const totalPurchases = await this.purchaseModel.countDocuments(where);
     return totalPurchases;
+  }
+
+  async getCreditCards(limit: number): Promise<CreditCard[]> {
+    this.logger.log('PurchaseService', `Fetching Credit Card List`);
+    return await this.externalApiService.getCreditCards(limit);
   }
 }
--- app/src/purchase/schemas/purchase.schema.ts
+++ app/src/purchase/schemas/purchase.schema.ts
@@ -1,18 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Category } from 'app/src/product/enum';
 import mongoose, { SchemaTypes, Types } from 'mongoose';
+
+interface Product {
+  _id: Types.ObjectId;
+  name: string;
+  price: number;
+  category: Category;
+}
 
 @Schema({ timestamps: true, versionKey: false })
 export class Purchase {
   _id: mongoose.Types.ObjectId;
 
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'Product' })
-  product: Types.ObjectId;
+  @Prop({ type: Object })
+  product: Product;
 
   @Prop({ default: Date.now })
   date: Date;
 
   @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
-  user: Types.ObjectId;
+  purchasedBy: Types.ObjectId;
 }
 
 export const PurchaseSchema = SchemaFactory.createForClass(Purchase);
